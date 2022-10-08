034410105e8edf75d324b8b1a76bfe7a3d245768
Niki <NikiNoel1218@gmail.com>
2022-10-08 19:02:34 -0400

    USGS earthquake data

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,8 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Alternative code to create map
 // Create the map object with a center and zoom level.
@@ -24,32 +23,66 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: API_KEY
 });
+
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark,
+    Satellite: satelliteStreets
+  };
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-// circleMarker from https://leafletjs.com/reference-1.6.0.html#circlemarker
-let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+// //  Add a marker to the map for Los Angeles, California.
+// // circleMarker from https://leafletjs.com/reference-1.6.0.html#circlemarker
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
 // L.geoJson(data, {
 //    pointToLayer: function(feature, latlng) {
@@ -65,9 +98,23 @@ let sanFranAirport =
 //    }
 // }).addTo(map);
 
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
-     }
-}).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+//      }
+// }).addTo(map);
 
+
+// // link to airport geoJSON URL from github
+// let airportData = "https://raw.githubusercontent.com/NikiNoel/Leaflet_Earthquake_Challenge/main/static/data/majorAirports.json"
+
+// Retrieve the earthquake GeoJSON data from USGS website.
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+let earthquakeData = ("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
+  
+// Grabbing our GeoJSON data.
+d3.json(earthquakeData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
