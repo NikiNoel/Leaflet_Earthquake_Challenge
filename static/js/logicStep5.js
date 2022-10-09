9ab86b222f062ecaf9135e3fcc6285ad474c4145
Niki <NikiNoel1218@gmail.com>
2022-10-09 16:56:12 -0400

    complete through step 5

--- static/js/logicStep5.js
+++ static/js/logicStep5.js
@@ -49,12 +49,13 @@ let map = L.map('mapid', {
 // This overlay will be visible all the time.
 let overlays = {
     Earthquakes: earthquakes
-  };
+};
 
-  // Pass our map layers into our layers control and add the layers control to the map.
+// Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps, overlays, {
-    collapsed: false}
-    ).addTo(map);
+    collapsed: false
+}
+).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
@@ -130,30 +131,41 @@ d3.json(earthquakeData).then(function (data) {
         }
     }).addTo(earthquakes);
 
+    // Create a legend control object.
+    let legend = L.control({
+        position: "bottomright"
+    });
+
+    // Then add all the details for the legend.
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+
+
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        const colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"
+        ];
+
+        // Looping through our intervals to generate a label with a colored square for each interval.
+        for (var i = 0; i < magnitudes.length; i++) {
+            console.log(colors[i]);
+            div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i> " +
+                magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+        }
+        return div;
+   
+    };
+
+    legend.addTo(map);
 
 
     // no data beyond this point
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
